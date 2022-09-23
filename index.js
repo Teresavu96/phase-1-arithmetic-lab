50c16d6e785af2280f17623d49fc82440ada6cd0
Anh Thu Vu <teresavu96@gmail.com>
2022-09-23 14:30:42 -0700

    commit tree 81b5b0636ff0fcd13fd05ebd6c977ad26b299e93

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random());
+const num3 = 29
+const num4 = 5
+const mod = num3 % num4;
+const max = Math.max(3,10,20);
